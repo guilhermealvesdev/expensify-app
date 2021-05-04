ca10f1f5eac9ab90a8e75b36b4592f9e62cf91a1
guilhermealvesdev <guilherme.alves.trash@gmail.com>
2021-05-03 22:51:01 -0300

    Aulas 74 a 77.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,50 @@
 ﻿import React from 'react';
 import ReactDOM from 'react-dom';
-import IndecisionApp from './components/IndecisionApp'
+import {BrowserRouter, Route} from 'react-router-dom';
 import 'normalize-css/normalize.css'; // Importando normalize.css do node_modules
 import './styles/style.scss';
 
-ReactDOM.render(<IndecisionApp />, document.getElementById("app"));
+const ExpenseDashboardPage = () => {
+    return (
+        <div>
+            <p>Dashboard</p>
+        </div>
+    )
+}
+
+const AddExpensePage = () => {
+    return (
+        <div>
+            <p>Add</p>
+        </div>
+    )
+}
+
+const EditExpensePage = () => {
+    return (
+        <div>
+            <p>Edit</p>
+        </div>
+    )
+}
+
+const HelpPage = () => {
+    return (
+        <div>
+            <p>Help</p>
+        </div>
+    )
+}
+
+const routes = (
+    <BrowserRouter>
+        <div>
+            <Route path="/" component={ExpenseDashboardPage} exact={true} />
+            <Route path="/create" component={AddExpensePage} />
+            <Route path="/edit" component={EditExpensePage} />
+            <Route path="/help" component={HelpPage} />
+        </div>
+    </BrowserRouter>
+)
+
+ReactDOM.render(routes, document.getElementById("app"));
