a18ea5b2052efaad0a66f810c0fd9ec00b4c3e2d
guilhermealvesdev <guilherme.alves.trash@gmail.com>
2021-05-24 23:44:21 -0300

    Aulas 83 a 86.

---
+++ src/playground/redux-101.js
@@ -0,0 +1,47 @@
+﻿import { createStore } from 'redux';
+
+const store = createStore((state = { count:0 }, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return {
+                count: state.count + 1
+            };
+        case 'DECREMENT':
+            return {
+                count: state.count - 1
+            }
+        case 'RESET':
+            return {
+                count: 0
+            }
+        default:
+            return state;
+    }
+});
+
+console.log(store.getState());
+
+
+//Incrementar
+store.dispatch({
+    type: "INCREMENT"
+});
+
+//Decrementar
+store.dispatch({
+    type: "INCREMENT"
+});
+
+//Deixar para 0
+store.dispatch({
+    type: "RESET"
+});
+
+//Decrementar
+store.dispatch({
+    type: "DECREMENT"
+});
+
+
+
+console.log(store.getState());
