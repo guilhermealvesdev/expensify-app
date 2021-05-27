85dc55b35a1cdc62b86db09566f0615f896b34de
guilhermealvesdev <guilherme.alves.trash@gmail.com>
2021-05-26 21:59:04 -0300

    Aulas 88 e 89.

---
+++ src/playground/destructuring.js
@@ -0,0 +1,24 @@
+﻿//Arrays
+const menu = ["Café (quente)", "R$ 2,00", "R$ 2,50", "R$2,75"];
+
+const [item, ,preco2, preco3] = menu
+
+console.log(`Um ${item} grande custa ${preco3}`);
+
+
+
+
+
+
+//Object
+const book = {
+    title: 'Ego is the enemy',
+    author: 'Ryan Holiday',
+    publisher: {
+        name: 'Penguin'
+    }
+}
+
+const {name: publisherName = "Self-published"} = book.publisher;
+
+console.log(`${publisherName}.`);
