6e695577b713772b8615700d0ca114b5558b54f5
guilhermealvesdev <guilherme.alves.trash@gmail.com>
2021-06-11 00:13:42 -0300

    Aula 100

---
+++ src/playground/hoc.js
@@ -0,0 +1,36 @@
+﻿//HIGHER ORDER COMPONENTS
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Info = (props) => {
+    return (
+        <div>
+            <h1>Info</h1>
+            <p>The info is: {props.isAdmin}</p>
+        </div>
+    )
+};
+
+const withAdminWarning = (WrappedComponent) => {
+    return (props) => (
+        <div>
+            {props.isAdmin && <p>ISSO É INFORMAÇÃO PRIVADA. NÃO COMPARTILHAR.</p>}
+            <WrappedComponent {...props} />
+        </div>
+    )
+}
+
+const requireAuthentication = (WrappedComponent) => {
+    return (props) => (
+        <div>
+            {props.isAuthenticated ? <WrappedComponent {...props} /> : <p>Por favor, logue para ver isto.</p>}
+        </div>
+    )
+}
+
+const AdminInfo = withAdminWarning(Info);
+const AuthInfo = requireAuthentication(Info);
+
+//ReactDOM.render(<AdminInfo isAdmin={true} info="detail" />, document.querySelector('#app'))
+ReactDOM.render(<AuthInfo isAuthenticated={false} />, document.querySelector('#app'))
